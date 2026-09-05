d491baf1b8577c5bb9f0fc52110dc18f0eaf0c61
arpitverma90 <vermaarp2361@gmail.com>
2026-09-05 15:47:41 +0530

    Fix Pages build entry point

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/teacher-s_day/',
+  build: {
+    rollupOptions: {
+      input: 'src/index.html',
+    },
+  },
   plugins: [react()],
 })
